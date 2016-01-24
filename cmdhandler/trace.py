cd18b755a3538b1fcf193ba02a12b8f6f248dea0
arturo182 <arturo182@tlen.pl>
2016-01-24 17:39:42 +0100

    Add trace handler

---
+++ cmdhandler/trace.py
@@ -0,0 +1,36 @@
+class trace:
+    def __init__(self, bot):
+        bot.add_event_handler("trace", self.muc_trace)
+        self.bot = bot
+        bot.register_help('trace',
+            'traces a host',
+            'usage: !trace host_to_trace')
+
+    def trace(self, host):
+        import subprocess
+        import locale
+
+        args = ['mtr', host, '--report-wide', '-c', '1']
+        proc = subprocess.Popen(args, stdout=subprocess.PIPE)
+        out, err = proc.communicate()
+        enc = locale.getdefaultlocale()[1]
+        out = out.decode(enc)
+
+        lines = out.split('\n')
+        if len(lines) > 11:
+            return '\n'.join(lines[:10] + ['--snip--'] + lines[-2:-1])
+
+        return out
+
+    def muc_trace(self, msg):
+        msg = msg[1]
+        host = msg['body'].strip()
+
+        if host == '':
+            body = "Please specify a host, see !help trace"
+        else:
+            body = ''.join(["Tracing ", host, '\n', self.trace(host)])
+
+        self.bot.send_message(mto=msg['from'].bare,
+            mbody=body,
+            mtype='groupchat')
